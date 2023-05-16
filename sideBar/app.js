d175d6f09149eaf7d88320261bbc2cb3b048ae9e
KANE <mahamadoudaba.kane@gmail.com>
2023-05-17 00:32:26 +0200

    ajout projet sideBar

--- sideBar/app.js
+++ sideBar/app.js
@@ -1,12 +1,17 @@
-const container = document.querySelector(".container")
+const container = document.querySelector(".container");
 const hide = document.querySelector(".cross");
-hide.addEventListener("click", function(){
-    container.style.transform = "translateX(-100%)";
+
+hide.addEventListener("click", function() {
+  container.style.transform = "translateX(-100%)";
   container.style.transition = "transform 0.5s";
-})
+});
 
 const display = document.querySelector(".toggle");
-display.addEventListener("click", function(){
+display.addEventListener("click", function() {
+  if (container.style.transform === "translateX(-100%)") {
     container.style.transform = "translateX(0%)";
+  } else {
+    container.style.transform = "translateX(-100%)";
+  }
   container.style.transition = "transform 0.5s";
-})
+});
